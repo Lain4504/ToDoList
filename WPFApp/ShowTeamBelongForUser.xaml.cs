f72dcef9092685eee4396d922f31e84d3a420cf2
Your Name <you@example.com>
2024-10-18 19:16:37 +0700

    Screen Show Team Belong for User

---
+++ WPFApp/ShowTeamBelongForUser.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using WPFApp.ViewModel;
+
+namespace WPFApp
+{
+    public partial class ShowTeamBelongForUser : Window
+    {
+        private UserViewModel userViewModel;
+
+        public ShowTeamBelongForUser()
+        {
+            InitializeComponent();
+            userViewModel = new UserViewModel();
+            DataContext = userViewModel; // Set the DataContext to the instance
+        }
+
+        private async void LoadTeamsButton_Click(object sender, RoutedEventArgs e)
+        {
+            int userId = 1; // Replace with the actual user ID or get it from user input
+            await userViewModel.LoadUserTeams(userId);
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(); // Logic to go back or close the window
+        }
+    }
+}
